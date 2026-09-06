abd4ec723cb93b0e0128c06e1a1fa0d82a07e42e
p1k2i <pro100kryto.original@gmail.com>
2026-09-06 14:43:03 +0100

    Configure for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/explainshot-site/',
+  base: process.env.VERCEL ? '/' : '/explainshot-site/',
   plugins: [
     vue(),
     vueDevTools(),
